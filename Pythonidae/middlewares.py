cc504b9dd1201eedab4b53e7104bbbf362efe656
dawitnida <dchonch@gmail.com>
2015-02-11 19:29:34 +0200

    Documentation Added.

--- Pythonidae/middlewares.py
+++
@@ -1,7 +0,0 @@
-""" Development of Web Applications and Web Services
-
-"""
-
-__author__ = "Dawit Nida (dawit.nida@abo.fi)"
-__date__ = "Date: 1.11.2014"
-__version__ = "Version: "
--- YAAS/report/static/__init__.py
+++
@@ -1,7 +0,0 @@
-""" Development of Web Applications and Web Services
-
-"""
-
-__author__ = "Dawit Nida (dawit.nida@abo.fi)"
-__date__ = "Date: 12.10.2014"
-__version__ = "Version: "
